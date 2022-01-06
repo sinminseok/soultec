fe60ae24bf77acf573376159942903fcfcdac158
FlutLab.io <>
2022-01-06 14:48:55 +0000

    Autosave

--- lib/App/home_page.dart
+++ lib/App/home_page.dart
@@ -119,7 +119,7 @@ class _Home_pageState extends State<Home_page> {
             onTap: () {
               //data push
               print('click');
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Filling(l: this._select_value)));
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Filling(l: _select_value)));
             },
             child: Container(
               width: size.width * 0.8,
