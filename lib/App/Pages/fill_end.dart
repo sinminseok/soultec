a1d80751520c95466bd0831576cdb81c8b5a0053
FlutLab.io <>
2022-01-06 14:27:28 +0000

    Autosave

--- lib/App/Pages/fill_end.dart
+++ lib/App/Pages/fill_end.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:soultec/App/widgets/drawer.dart';
-import 'package:soultec/App/Pages/fill_end.dart';
 
 import 'package:soultec/auth.dart';
 
 import '../../constants.dart';
 
-class FillStop extends StatefulWidget {
+class FillEnd extends StatefulWidget {
   //late final String? uid;
 
   //Filling({required this.uid});
 
   @override
-  _FillStopState createState() => _FillStopState();
+  _FillEndState createState() => _FillEndState();
 }
 
-class _FillStopState extends State<FillStop> {
+class _FillEndState extends State<FillEnd> {
   int pageIndex = 0;
 
   String? _select_value;
@@ -39,18 +38,18 @@ class _FillStopState extends State<FillStop> {
       child: SingleChildScrollView(
         child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
-            "주 입 멈 춤",
+            "주 입 완 료",
             style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
           ),
           SizedBox(
             height: size.height * 0.1,
           ),
           Text(
-            "[김영솔]님 환영합니다\n",
+            "[김영솔]님 환영합니다.\n",
             style: TextStyle(fontSize: 22),
           ),
           Text(
-            "[30]리터로 주입을 원하십니다.",
+            "[30]리터로 주입이 끝났습니다.",
             style: TextStyle(fontSize: 20),
           ),
           SizedBox(
@@ -60,35 +59,26 @@ class _FillStopState extends State<FillStop> {
           SizedBox(
             height: size.height * 0.02,
           ),
-          Text("주입 중입니다.", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue)),
-          Text("계속해서 주입해주세요.", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
           SizedBox(
-            height: size.height * 0.10,
+            height: size.height * 0.03,
           ),
-          InkWell(
-            onTap: () {
-              //data push
-
-              Navigator.push(context, MaterialPageRoute(builder: (context) => FillEnd()));
-            },
-            child: Container(
-              width: size.width * 0.7,
-              height: 45,
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(6), color: kPrimaryColor),
-              padding: EdgeInsets.symmetric(vertical: 8),
-              alignment: Alignment.center,
-              child: Text('정 량', style: TextStyle(color: Colors.white, fontSize: 20)),
+          Text("리터 [30] L", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30.0),
+            child: Image(
+              image: AssetImage('assets/images/cartoonimg.png'),
+              width: 140,
             ),
           ),
           SizedBox(
             height: size.height * 0.03,
           ),
-          Text("리터 [0] L", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-          SizedBox(
-            height: size.height * 0.03,
-          ),
-          Text("정량을 맞추시려면", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          Text("정량 버튼을 터치해 주세요.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text("주입이 완료되었습니다.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text("노즐을 주입기에 걸어주세요.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          Text("수고하셨습니다. 안녕하가세요.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         ]),
       ),
     );
