978be0772d44dd8fcf4b6b4115b324a8d68abd57
FlutLab.io <>
2022-01-06 14:46:24 +0000

    Autosave

--- lib/App/Pages/fill_end.dart
+++ lib/App/Pages/fill_end.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:soultec/App/widgets/drawer.dart';
 
+import '../../constants.dart';
+
 class FillEnd extends StatefulWidget {
   //late final String? uid;
 
