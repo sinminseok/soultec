7c82e281026da6606dc61c2ec90e990ccd904a1f
FlutLab.io <>
2022-01-06 23:40:11 +0000

    Autosave

--- lib/App/Pages/fill_ing.dart
+++ lib/App/Pages/fill_ing.dart
@@ -9,7 +9,7 @@ class Filling extends StatefulWidget {
   final String? liter;
   Filling({@required this.liter});
 
-  print(this.liter);
+  //print(this.liter);
   @override
   _FillingState createState() => _FillingState();
 }
