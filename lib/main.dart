f46816da6653956c1a92b74f58435e2b548576fa
FlutLab.io <>
2022-01-06 10:58:45 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -12,10 +11,17 @@ import 'Data/database.dart';
 import 'auth.dart';
 import 'constants.dart';
 
-void main () async{
+void main() async {
   //firebase를 사용 하기 위해선 main 메소드에 알려줘야한다.
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCxWCh4uCHs2al4WAWjFC12szwZcG6yq6I", // Your apiKey
+      appId: "1:467091027615:android:b85711bd7771fe321e94aa", // Your appId
+      messagingSenderId: "467091027615", // Your messagingSenderId
+      projectId: "soultec-6f71d", // Your projectId
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -28,12 +34,11 @@ class MyApp extends StatelessWidget {
       value: AuthService().userState,
       initialData: null,
       child: MaterialApp(
-        theme: ThemeData(
-          primaryColor: kPrimaryColor,
-          // textTheme: GoogleFonts.roboTextTheme(Theme.of(context).textTheme)
-        ),
-        home: Start_page()
-      ),
+          theme: ThemeData(
+            primaryColor: kPrimaryColor,
+            // textTheme: GoogleFonts.roboTextTheme(Theme.of(context).textTheme)
+          ),
+          home: Start_page()),
     );
   }
 }
