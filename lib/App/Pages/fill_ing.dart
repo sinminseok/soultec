d12b66217bfa99f8685458c1350d07e9f6c8d7d8
FlutLab.io <>
2022-01-06 13:42:00 +0000

    Autosave

---
+++ lib/App/Pages/fill_ing.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:soultec/App/widgets/drawer.dart';
+
+import 'package:soultec/auth.dart';
+
+import '../constants.dart';
+
+class Filling extends StatefulWidget {
+  late final String? uid;
+
+  Filling({required this.uid});
+
+  @override
+  _FillingState createState() => _FillingState();
+}
+
+class _FillingState extends State<Filling> {
+  int pageIndex = 0;
+
+  String? _select_value;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        drawer: My_Drawer(),
+        appBar: AppBar(
+          backgroundColor: kPrimaryColor,
+          elevation: 0,
+        ),
+        body: getBody(size));
+  }
+
+  getBody(Size size) {
+    return Align(
+      alignment: Alignment.center,
+      child: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            "주입중",
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: size.height * 0.1,
+          ),
+          Text(
+            "[김영솔]님 환영합니다\n\n[30]L로 주입을 원하십니다.",
+          ),
+          SizedBox(
+            height: size.height * 0.02,
+          ),
+          Container(width: 500, child: Divider(color: Colors.black, thickness: 1.0)),
+          SizedBox(
+            height: size.height * 0.08,
+          ),
+          Text("주유 중입니다.\n계속해서 주유하세요.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: size.height * 0.08,
+          ),
+          Text("리터[0]L"),
+          SizedBox(
+            height: size.height * 0.01,
+          ),
+          Text("주입 중입니다.\n잠시만 기다려 주시기 바랍니다.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        ]),
+      ),
+    );
+  }
+
+  selectedTap(index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+}
--- lib/App/home_page.dart
+++ lib/App/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:soultec/App/widgets/drawer.dart';
+import 'package:soultec/App/Pages/fill_ing.dart';
 
 import 'package:soultec/auth.dart';
 
@@ -19,7 +20,10 @@ class _Home_pageState extends State<Home_page> {
   int pageIndex = 0;
   TextEditingController inputController = TextEditingController();
 
-  final values = ["가득", "리터"];
+  final values = [
+    "가득",
+    "리터"
+  ];
   var select_firest = "가득";
   String? _select_value;
 
@@ -39,112 +43,96 @@ class _Home_pageState extends State<Home_page> {
     return Align(
       alignment: Alignment.center,
       child: SingleChildScrollView(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image(
+            image: AssetImage('assets/images/mainimg.png'),
+            width: 140,
+          ),
+          Text("빠르고 편한 주입"),
+          Text(
+            "스마트필",
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          Text("기사님 환영합니다"),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          Text("페어링이 정상적으로 연결 되었습니다!"),
+          Text("리터랑을 설정해주십시요"),
+          SizedBox(
+            height: size.height * 0.01,
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image(
-                image: AssetImage('assets/images/mainimg.png'),
-                width: 140,
-              ),
-              Text("빠르고 편한 주입"),
-              Text(
-                "스마트필",
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              Text("기사님 환영합니다"),
-              SizedBox(
-                height: size.height * 0.03,
-              ),
-              Text("페어링이 정상적으로 연결 되었습니다!"),
-              Text("리터랑을 설정해주십시요"),
-              SizedBox(height: size.height*0.01,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  DropdownButton(
-                    elevation: 16,
-                    underline: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(width:0.5, color: Colors.black38)
-                        )
-                    ),
-                    hint: Text(
-                      "가득/리터",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 14,
-
-                      ),
-                    ),
-                    value: _select_value,
-                    dropdownColor: Colors.white,
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-
-                    onChanged: (val) => setState(() => _select_value = val.toString()),
-                    items: [
-                      for (var val in values)
-                        DropdownMenuItem(
-                          value: val,
-                          child: SizedBox(
-                            child: Text(
-                              val.toString(),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: Colors.black87,
-                              ),
-                            ),
+              DropdownButton(
+                elevation: 16,
+                underline: Container(decoration: BoxDecoration(border: Border.all(width: 0.5, color: Colors.black38))),
+                hint: Text(
+                  "가득/리터",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 14,
+                  ),
+                ),
+                value: _select_value,
+                dropdownColor: Colors.white,
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                onChanged: (val) => setState(() => _select_value = val.toString()),
+                items: [
+                  for (var val in values)
+                    DropdownMenuItem(
+                      value: val,
+                      child: SizedBox(
+                        child: Text(
+                          val.toString(),
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black87,
                           ),
                         ),
-                    ],
-
-                  ),
-                  SizedBox(width: size.width*0.05),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 5),
-                    padding:
-                    EdgeInsets.symmetric(horizontal: 5, vertical: 1),
-                    width: size.width * 0.4,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: Colors.black.withAlpha(30)),
-                    child: TextFormField(
-                      controller: inputController,
-                      decoration: InputDecoration(
-                          hintText: '가득/리터',
-                          border: InputBorder.none),
+                      ),
                     ),
-                  ),
                 ],
               ),
-              SizedBox(height: size.height*0.03,),
-              InkWell(
-                onTap: (){
-                  //data push
-                  print('click');
-
-                },
-                child: Container(
-                  width: size.width * 0.8,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: kPrimaryColor),
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  alignment: Alignment.center,
-                  child: Text('주입시작',
-                      style:
-                      TextStyle(color: Colors.white, fontSize: 16)),
+              SizedBox(width: size.width * 0.05),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 1),
+                width: size.width * 0.4,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), color: Colors.black.withAlpha(30)),
+                child: TextFormField(
+                  controller: inputController,
+                  decoration: InputDecoration(hintText: '가득/리터', border: InputBorder.none),
                 ),
               ),
-
-            ]),
+            ],
+          ),
+          SizedBox(
+            height: size.height * 0.03,
+          ),
+          InkWell(
+            onTap: () {
+              //data push
+              print('click');
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Filling()));
+            },
+            child: Container(
+              width: size.width * 0.8,
+              height: 60,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), color: kPrimaryColor),
+              padding: EdgeInsets.symmetric(vertical: 20),
+              alignment: Alignment.center,
+              child: Text('주입시작', style: TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+          ),
+        ]),
       ),
     );
   }
