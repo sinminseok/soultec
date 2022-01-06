6c6c08d3f9421e38fed310b599a0a84fa71321e8
FlutLab.io <>
2022-01-06 23:25:32 +0000

    Autosave

--- lib/App/Pages/fill_ing.dart
+++ lib/App/Pages/fill_ing.dart
@@ -6,7 +6,7 @@ import 'package:soultec/App/Pages/fill_stop.dart';
 import '../../constants.dart';
 
 class Filling extends StatefulWidget {
-  late final String? l;
+  final String? l;
   print(l);
   Filling({@required this.l});
 
